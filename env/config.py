845ecfe589640446bdb611416aff1395ee53d2e4
Bruno Peselli <brunopeselli@gmail.com>
2021-07-25 22:44:57 -0300

    Minor fixes

--- env/config.py
+++ env/config.py
@@ -2,6 +2,8 @@
 URL_BEPINEX = "https://github.com/BepInEx/BepInEx/releases/download/v5.4.13/BepInEx_x64_5.4.13.0.zip"
 URL_KINO = "https://github.com/trbflxr/kino/releases/download/v2.5.7/release_2.5.7.zip"
 URL_NVA = "https://cdn.discordapp.com/attachments/713352414547148841/851924540027306055/NVA.rar"
+URL_TUNES = "https://github.com/pzzzl/kino-installer/raw/main/assets/tunes.zip"
+URL_VISUALS = "https://github.com/pzzzl/kino-installer/raw/main/assets/visuals.zip"
 
 # Folders
 CARX_FOLDER = "C:/Program Files (x86)/Steam/steamapps/common/CarX Drift Racing Online"
--- kino_installer.py
+++ kino_installer.py
@@ -3,16 +3,31 @@ from src.download import Download
 from src.unzip import Unzip
 from src.utils import Directory, File
 
+# DOWNLOAD & INSTALL BEPINEX
 BEPINEX_ZIP = Download(url=URL_BEPINEX).start()
 Unzip(path=BEPINEX_ZIP, destination=CARX_FOLDER).start()
 File(path=BEPINEX_ZIP).delete()
 
+# DOWNLOAD & INSTALL KINO
 KINO_ZIP = Download(url=URL_KINO).start()
 Directory(path=PLUGINS_FOLDER).check()
-Directory(path=KN_BASE_FOLDER).check()
-Directory(path=TUNES_FOLDER).check()
-Directory(path=VISUALS_FOLDER).check()
 Unzip(path=KINO_ZIP, destination=PLUGINS_FOLDER).start()
 File(path=KINO_ZIP).delete()
 
+# CREATE KINO FOLDER
+Directory(path=KN_BASE_FOLDER).check()
+
+# DOWNLOAD & INSTALL CUSTOM TUNES
+TUNES_ZIP = Download(url=URL_TUNES).start()
+Directory(path=TUNES_FOLDER).check()
+Unzip(path=TUNES_ZIP, destination=TUNES_FOLDER).start()
+File(path=TUNES_ZIP).delete()
+
+# DOWNLOAD & INSTALL CUSTOM VISUALS (LIVERIES)
+VISUALS_ZIP = Download(url=URL_VISUALS).start()
+Directory(path=VISUALS_FOLDER).check()
+Unzip(path=VISUALS_ZIP, destination=VISUALS_FOLDER).start()
+File(path=VISUALS_ZIP).delete()
+
+# DOWNLOAD NVA
 NVA_ZIP = Download(url=URL_NVA).start()
