825a191e02feceae6ab53f67e28456ab3350e834
Bruno Peselli <brunopeselli@gmail.com>
2021-08-15 12:44:56 -0300

    Update

---
+++ version/latest.py
@@ -0,0 +1,2 @@
+KINO = "https://github.com/trbflxr/kino/releases/download/v3.0.1/release_3.0.1.zip"
+BEPINEX = "https://github.com/BepInEx/BepInEx/releases/download/v5.4.15/BepInEx_x64_5.4.15.0.zip"
